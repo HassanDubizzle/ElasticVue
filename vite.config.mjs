a2b80760a4b5313dd367c6456b9717b81c0b0f6a
Hassan Abouelela <hassan.abouelela@dubizzle.com>
2026-08-04 09:42:19 +0400

    Fix Windows Builds
    Signed-off-by: Hassan Abouelela <hassan.abouelela@dubizzle.com>

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
-import { resolve, dirname } from 'node:path'
+import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'url'
 
 function removeDataTestid (node) {
@@ -15,6 +15,11 @@ const prod = process.env.NODE_ENV === 'production'
 
 export default defineConfig({
   base: process.env.VITE_APP_PUBLIC_PATH || '/',
+  server: {
+    watch: {
+      ignored: ['**/src-tauri/target/**']
+    }
+  },
   plugins: [
     vue({
       template: {
